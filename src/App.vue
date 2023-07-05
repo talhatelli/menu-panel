55f5c5e3c72d88b38388be39076ad3d8425d5719
talhatelli <ahmet.telli@tsoft.club>
2023-07-05 22:18:45 +0300

    page layout provided

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,112 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <a-layout>
+    <a-layout-header class="header">
+      <div class="icon logo" />
+      <div class="icon icon-user" />
+      <a-menu v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal" :style="{lineHeight: '64px'}">
+        <a-menu-item key="1">nav 1</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <a-layout style="height: 100vh; background: #441717">
+      <a-layout-sider height="100%" width="200">
+        <a-menu
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          mode="inline"
+          :style="{height: '100%', background: '#001529', borderRight: 0, color: 'white'}"
+        >
+          <a-sub-menu key="sub1">
+            <template #title>
+              <span>
+                <user-outlined />
+                subnav 1
+              </span>
+            </template>
+            <a-menu-item key="1">option1</a-menu-item>
+            <a-menu-item key="2">option2</a-menu-item>
+            <a-menu-item key="3">option3</a-menu-item>
+            <a-menu-item key="4">option4</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <template #title>
+              <span>
+                <laptop-outlined />
+                subnav 2
+              </span>
+            </template>
+            <a-menu-item key="5">option5</a-menu-item>
+            <a-menu-item key="6">option6</a-menu-item>
+            <a-menu-item key="7">option7</a-menu-item>
+            <a-menu-item key="8">option8</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <template #title>
+              <span>
+                <notification-outlined />
+                subnav 3
+              </span>
+            </template>
+            <a-menu-item key="9">option9</a-menu-item>
+            <a-menu-item key="10">option10</a-menu-item>
+            <a-menu-item key="11">option11</a-menu-item>
+            <a-menu-item key="12">option12</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <!-- content -->
+      <a-layout style="padding: 0 24px 24px">
+        <a-layout-content :style="{background: '#fff', padding: '24px', margin: 0, minHeight: '280px'}">
+          Content
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script lang="ts">
+import {UserOutlined, LaptopOutlined, NotificationOutlined} from "@ant-design/icons-vue"
+import {defineComponent, ref} from "vue"
+export default defineComponent({
+  components: {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined
+  },
+  setup() {
+    return {
+      selectedKeys1: ref<string[]>(["2"]),
+      selectedKeys2: ref<string[]>(["1"]),
+      collapsed: ref<boolean>(false),
+      openKeys: ref<string[]>(["sub1"])
+    }
+  }
+})
+</script>
+<style>
+.icon {
+  background-size: 40px 40px; /* İstenilen genişlik ve yükseklik değerlerini burada belirtin */
+  width: 40px; /* Logo boyutunu ayarlayın */
+  height: 40px;
+  float: left;
+  margin: 16px 24px 16px 0;
+  border-radius: 50%;
 }
-
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  background-image: url("icons/logo.jpeg");
+}
+.icon-user {
+  background-image: url("icons/user.png");
+  float: right;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.admin-logo {
+  float: right;
+  margin: 16px 0 16px 24px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.site-layout-background {
+  background: #fff;
 }
 </style>
