a4545c3319432cdbb406fd8b204ec765b43ca69b
Ahmet Talha Telli <ahmet.telli@tsoft.club>
2023-08-03 16:31:36 +0300

    menu item list data controler

--- src/views/MenuItemListView.vue
+++ src/views/MenuItemListView.vue
@@ -8,78 +8,88 @@
   </a-table>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
 import { menuItemListStore } from "../stores/menuItemListStore";
-import { onMounted, computed } from "vue";
 
-const store = menuItemListStore();
-const getUsers = computed(() => {
-  return store.getUsers;
+export default defineComponent({
+  setup() {
+    const store = menuItemListStore();
+    const getUsers = ref([]);
+    const users = ref([]);
+    const data = ref([]);
+    onMounted(async () => {
+      const users = await store.fetchUsers();
+      data.value = users.map((item, index) => {
+        return {
+          key: (index + 1).toString(),
+          name: item.name,
+          age: item.age,
+          address: item.address,
+          tags: item.tags,
+        };
+      });
+    });
+
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'PRICE',
+        dataIndex: 'address',
+        key: 'address 1',
+        ellipsis: true,
+      },
+      {
+        title: 'DATE',
+        dataIndex: 'address',
+        key: 'address 2',
+        ellipsis: true,
+      },
+      {
+        title: 'STATUS',
+        dataIndex: 'address',
+        key: 'address 3',
+        ellipsis: true,
+      },
+      {
+        title: 'ACTIONS',
+        dataIndex: 'address',
+        key: 'address 4',
+        ellipsis: true,
+      },
+    ];
+
+    // const data = [
+    //   {
+    //     key: '1',
+    //     name: "sdesfbcfdf",
+    //     age: 32,
+    //     address: 'New York',
+    //     tags: ['nice', 'developer'],
+    //   },
+    //   {
+    //     key: '2',
+    //     name: 'Jim Green',
+    //     age: 42,
+    //     address: 'London No. Park',
+    //     tags: ['loser'],
+    //   },
+    //   {
+    //     key: '3',
+    //     name: 'Joe Black',
+    //     age: 32,
+    //     address: 'Sidney No. Park',
+    //     tags: ['cool', 'teacher'],
+    //   },
+    // ];
+
+    return {
+      columns,
+    };
+  },
 });
-const users = computed(() => {
-  return store.users;
-});
-onMounted(() => {
-  store.fetchUsers();
-  console.log("getUsers", getUsers)
-  console.log("users", users)
-  console.log("fetchUsers", store.fetchUsers)
-
-
-});
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'PRICE',
-    dataIndex: 'address',
-    key: 'address 1',
-    ellipsis: true,
-  },
-  {
-    title: 'DATE',
-    dataIndex: 'address',
-    key: 'address 2',
-    ellipsis: true,
-  },
-  {
-    title: 'STATUS',
-    dataIndex: 'address',
-    key: 'address 3',
-    ellipsis: true,
-  },
-  {
-    title: 'ACTIONS',
-    dataIndex: 'address',
-    key: 'address 4',
-    ellipsis: true,
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park, New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 2 Lake Park, London No. 2 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park, Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
 </script>
