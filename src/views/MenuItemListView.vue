770503decaea1a6e9298cbfac22ab479b8e0a8ac
Ahmet Talha Telli <ahmet.telli@tsoft.club>
2023-08-05 16:42:00 +0300

    Added data to the table

--- src/views/MenuItemListView.vue
+++ src/views/MenuItemListView.vue
@@ -1,9 +1,14 @@
 <template>
   <a-table :columns="columns" :data-source="data" style="height: 100vh;">
-    <template #bodyCell="{ column, text }">
-      <template v-if="column.dataIndex === 'name'">
-        <a>{{ text }}</a>
-      </template>
+    <template #imageSlot="{ record }">
+      <div>
+        <img :src="record.imageUrl" alt="Image" width="50" height="50" />
+      </div>
+    </template>
+    <template #nameSlot="{ text }">
+      <div class="name-cell">
+        {{ text }}
+      </div>
     </template>
   </a-table>
 </template>
@@ -15,81 +20,73 @@ import { menuItemListStore } from "../stores/menuItemListStore";
 export default defineComponent({
   setup() {
     const store = menuItemListStore();
-    const getUsers = ref([]);
-    const users = ref([]);
     const data = ref([]);
+
     onMounted(async () => {
-      const users = await store.fetchUsers();
-      data.value = users.map((item, index) => {
+      await store.fetchUsers();
+      const users = store.getUsers;
+      data.value = users.map((user) => {
         return {
-          key: (index + 1).toString(),
-          name: item.name,
-          age: item.age,
-          address: item.address,
-          tags: item.tags,
+          name: user.name,
+          address: user.description,
+          price: user.price,
+          date: user.createdAt,
+          status: user.isDeleted ? 'Deleted' : 'Active',
+          actions: 'detail edit',
+          imageUrl: user.imageUrl,
         };
       });
     });
 
     const columns = [
       {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
+        dataIndex: "imageUrl",
+        key: "image",
+        scopedSlots: { customRender: 'imageSlot' },
       },
       {
-        title: 'PRICE',
-        dataIndex: 'address',
-        key: 'address 1',
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        scopedSlots: { customRender: 'nameSlot' },
+      },
+      {
+        title: "PRICE",
+        dataIndex: "price",
+        key: "address 1",
         ellipsis: true,
       },
       {
         title: 'DATE',
-        dataIndex: 'address',
+        dataIndex: 'date',
         key: 'address 2',
         ellipsis: true,
       },
       {
         title: 'STATUS',
-        dataIndex: 'address',
+        dataIndex: 'status',
         key: 'address 3',
         ellipsis: true,
       },
       {
         title: 'ACTIONS',
-        dataIndex: 'address',
+        dataIndex: 'actions',
         key: 'address 4',
         ellipsis: true,
       },
     ];
 
-    // const data = [
-    //   {
-    //     key: '1',
-    //     name: "sdesfbcfdf",
-    //     age: 32,
-    //     address: 'New York',
-    //     tags: ['nice', 'developer'],
-    //   },
-    //   {
-    //     key: '2',
-    //     name: 'Jim Green',
-    //     age: 42,
-    //     address: 'London No. Park',
-    //     tags: ['loser'],
-    //   },
-    //   {
-    //     key: '3',
-    //     name: 'Joe Black',
-    //     age: 32,
-    //     address: 'Sidney No. Park',
-    //     tags: ['cool', 'teacher'],
-    //   },
-    // ];
-
     return {
       columns,
+      data,
     };
   },
 });
 </script>
+
+<style>
+.name-cell {
+  color: blue;
+  font-size: large;
+}
+</style>
